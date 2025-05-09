682c823ac10862b025901d6031cecbdf8913b54a
everett-alanHood <everettalan.hood@gmail.com>
2025-05-09 02:37:31 -0400

    booob
    bon

--- Assets/Scripts/BobController.cs
+++ Assets/Scripts/BobController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ArrowHit : MonoBehaviour
+public class BobController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
